2612a198543f6cc7b643bc9846618704c9be1bbc
finnelliott <f.j.elliott@icloud.com>
2021-06-07 21:27:29 +0100

    removing italian

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withTM({
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ['en-gb', 'it-it'],
+    locales: ['en-gb'],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'en-gb',
