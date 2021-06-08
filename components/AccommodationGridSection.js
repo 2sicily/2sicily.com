01de2b5f476cb737ca65240a4d64c07ca70aa3ea
finnelliott <f.j.elliott@icloud.com>
2021-06-08 08:54:12 +0100

    adding accommodation grid page

---
+++ components/AccommodationGridSection.js
@@ -0,0 +1,49 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { MailIcon, PhoneIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const people = [
+  {
+    name: 'Jane Cooper',
+    title: 'Paradigm Representative',
+    role: 'Admin',
+    email: 'janecooper@example.com',
+    telephone: '+1-202-555-0170',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  // More people...
+]
+
+export default function AccommodationGridSection({ props }) {
+  const router = useRouter()
+  return (
+    <div className="max-w-7xl mx-auto">
+    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {props.map((item) => (
+        <Link href={`${router.query.accommodation_type}/${item.uid}`}>
+        <li
+          key={item.uid}
+          className="hover:bg-brand-three cursor-pointer col-span-1 flex flex-col text-center bg-brand-white shadow border border-brand-zero p-4"
+        >
+        
+        {/* <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src={item.imageUrl} alt="" /> */}
+          <div className="flex-1 flex py-2">
+            <span className="flex-none text-gray-900 text-sm font-serif">{item.data.accommodation_name}</span>
+            <span className="flex-1"></span>
+            <span className="flex-none text-gray-900 text-sm font-light">{item.data.location_headline}</span>
+          </div>
+          <div className="flex-1 flex py-2">
+            <span className="flex-none text-gray-900 text-sm font-light">{item.data.sleeps} guests</span>
+            <span className="flex-1"></span>
+            <span className="flex-none text-gray-900 text-sm font-light">{item.data.location_headline}</span>
+          </div>
+        </li>
+        </Link>
+      ))}
+    </ul>
+    </div>
+    
+  )
+}
--- pages/places-to-stay/[accommodation_type]/index.js
+++ pages/places-to-stay/[accommodation_type]/index.js
@@ -1,5 +1,72 @@
-export default function Page() {
-    return (
-        <div>Page</div>
-    )
+import SliceZone from "next-slicezone";
+import { useGetStaticPaths, useGetStaticProps } from 'next-slicezone/hooks'
+import { queryRepeatableDocuments } from '../../../utils/queries';
+import { Client, manageLocal } from '../../../utils/prismicHelpers';
+
+import resolver from "../../../sm-resolver.js";
+import Layout from "../../../components/Layout";
+import AccommodationGridSection from "../../../components/AccommodationGridSection";
+
+const Page = (props) => {
+  return (
+    <Layout props={props.doc} menu={props.menu}>
+      <SliceZone slices={props.doc.data.body} resolver={resolver} />
+      <AccommodationGridSection props={props.documents} />
+    </Layout>
+  )
+};
+
+// Fetch content from prismic
+///////////////////////
+
+export async function getStaticProps({
+  params,
+  locale,
+  locales,
+}) {
+  const client = Client();
+  const doc =
+    (await client.getByUID(
+      'grid_page',
+      params.accommodation_type,
+      { lang: locale }
+    )) || {};
+  const menu =
+    (await client.getSingle('menu', { lang: locale })) ||
+    {};
+  const documents = await queryRepeatableDocuments(
+    (doc) => doc.type === 'accommodation_page' && doc.data.accommodation_type === params.accommodation_type
+    );
+
+  const { currentLang, isMyMainLanguage } = manageLocal(locales, locale)
+
+  return {
+    props: {
+      menu,
+      doc,
+      lang:{
+        currentLang,
+        isMyMainLanguage,
+      },
+      documents
+    },
+  };
 }
+
+
+export async function getStaticPaths() {
+  const documents = await queryRepeatableDocuments(
+    (doc) => doc.type === 'grid_page'
+  );
+  return {
+    paths: documents.map((doc) => {
+      return { params: { accommodation_type: doc.uid }, locale: doc.lang };
+    }),
+    fallback: false,
+  };
+}
+
+
+export default Page;
+
+
