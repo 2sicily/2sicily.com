14ce69608d3e53755b619ef1a37c0c40270826d4
finnelliott <f.j.elliott@icloud.com>
2021-06-08 20:19:20 +0100

    adding accommodation grid section

--- components/AccommodationGridSection.js
+++ components/AccommodationGridSection.js
@@ -19,7 +19,7 @@ const people = [
 export default function AccommodationGridSection({ props }) {
   const router = useRouter()
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto p-4">
     <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {props.map((item) => (
         <Link href={`${router.query.accommodation_type}/${item.uid}`}>
