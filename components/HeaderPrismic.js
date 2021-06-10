e4851021c3d2e1b4887edb7a0bd9821d52c98f42
finnelliott <f.j.elliott@icloud.com>
2021-06-10 08:27:37 +0100

    adding desktop main navigation

--- components/HeaderPrismic.js
+++ components/HeaderPrismic.js
@@ -98,22 +98,31 @@ export default function Example({ menu }) {
                 </Link>
               </div>
               <div className="-mr-2 -my-2 md:hidden">
-                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-brand-three">
                   <span className="sr-only">Open menu</span>
                   <MenuIcon className="h-6 w-6" aria-hidden="true" />
                 </Popover.Button>
               </div>
-              <Popover.Group as="nav" className="hidden md:flex space-x-10 flex-row-reverse justify-center">
-                <Popover className="relative">
+              <Popover.Group as="nav" className="hidden md:flex space-x-10 justify-center">
+                {menu.data.body.map((item) => (
+                  item.slice_type === "Primary Pages" ?
+                  <Link href={`/${item.items[0].nav_link.uid}`}>
+                  <a className="text-base font-light capitalize text-gray-500 hover:text-gray-900">
+                    {item.items[0].link_text}
+                  </a>
+                  </Link>
+                  :
+                  item.slice_type === "Dropdown" ?
+                  <Popover className="relative">
                   {({ open }) => (
                     <>
                       <Popover.Button
                         className={classNames(
                           open ? 'text-gray-900' : 'text-gray-500',
-                          'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                          'px-2 group bg-white rounded-md inline-flex items-center text-base font-light hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-three'
                         )}
                       >
-                        <span>Solutions</span>
+                        <span>{item.primary.dropdown_label}</span>
                         <ChevronDownIcon
                           className={classNames(
                             open ? 'text-gray-600' : 'text-gray-400',
@@ -135,35 +144,21 @@ export default function Example({ menu }) {
                       >
                         <Popover.Panel
                           static
-                          className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
+                          className="absolute z-30 -ml-4 mt-3 transform px-2 w-screen max-w-xs sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
                         >
-                          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                          <div className="bg-white rounded-lg shadow-lg ring-1 ring-brand-three ring-opacity-100 overflow-hidden">
                             <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                              {solutions.map((item) => (
-                                <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                              {item.items.map((menu_item) => (
+                                <Link
+                                  href={`/${item.primary.dropdown_url}/${menu_item.third_level_link.uid}`}
                                 >
-                                  <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                                  <div className="ml-4">
-                                    <p className="text-base font-medium text-gray-900">{item.name}</p>
-                                    <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                                  </div>
-                                </a>
-                              ))}
-                            </div>
-                            <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                              {callsToAction.map((item) => (
-                                <div key={item.name} className="flow-root">
                                   <a
-                                    href={item.href}
-                                    className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
+                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                                  key={menu_item.third_level_link.uid}
                                   >
-                                    <item.icon className="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
-                                    <span className="ml-3">{item.name}</span>
+                                    <p className="text-base font-light text-gray-900">{menu_item.third_level_link_text}</p>
                                   </a>
-                                </div>
+                                </Link>
                               ))}
                             </div>
                           </div>
@@ -172,19 +167,11 @@ export default function Example({ menu }) {
                     </>
                   )}
                 </Popover>
-                {menu.data.body.map((item) => (
-                  <Link href={item.primary.link_text}>
-                  <a className="text-base font-light capitalize text-gray-500 hover:text-gray-900">
-                    {item.primary.link_text}
-                  </a>
-                  </Link>
+                  :
+                  "OOPS"
                 ))
                 }
                 
-                <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                  Docs
-                </a>
-
               </Popover.Group>
             </div>
           </div>
@@ -215,7 +202,7 @@ export default function Example({ menu }) {
                       />
                     </div>
                     <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-brand-three">
                         <span className="sr-only">Close menu</span>
                         <XIcon className="h-6 w-6" aria-hidden="true" />
                       </Popover.Button>
@@ -230,7 +217,7 @@ export default function Example({ menu }) {
                           className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                         >
                           <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                          <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                          <span className="ml-3 text-base font-light text-gray-900">{item.name}</span>
                         </a>
                       ))}
                     </nav>
@@ -238,18 +225,18 @@ export default function Example({ menu }) {
                 </div>
                 <div className="py-6 px-5 space-y-6">
                   <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                    <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                    <a href="#" className="text-base font-light text-gray-900 hover:text-gray-700">
                       Pricing
                     </a>
 
-                    <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                    <a href="#" className="text-base font-light text-gray-900 hover:text-gray-700">
                       Docs
                     </a>
                     {resources.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className="text-base font-medium text-gray-900 hover:text-gray-700"
+                        className="text-base font-light text-gray-900 hover:text-gray-700"
                       >
                         {item.name}
                       </a>
@@ -258,11 +245,11 @@ export default function Example({ menu }) {
                   {/* <div>
                     <a
                       href="#"
-                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-light text-white bg-indigo-600 hover:bg-indigo-700"
                     >
                       Sign up
                     </a>
-                    <p className="mt-6 text-center text-base font-medium text-gray-500">
+                    <p className="mt-6 text-center text-base font-light text-gray-500">
                       Existing customer?{' '}
                       <a href="#" className="text-indigo-600 hover:text-indigo-500">
                         Sign in
@@ -273,6 +260,7 @@ export default function Example({ menu }) {
               </div>
             </Popover.Panel>
           </Transition>
+          {JSON.stringify(menu.data)}
         </>
       )}
     </Popover>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 module.exports = {
   purge: ['./components/**/*.js', './pages/**/*.js', './slices/**/*.js', ],
   theme: {
-    colors: {
-      brand: {
-        zero: '#4458BE',
-        one: '#4B61D1',
-        two: '#7889DD',
-        three: '#EFF2FF',
-        four: '#F8F6F0',
-        five: '#DDBEA9',
-        six: '#CB997E',
-        white: '#FDFDFD',
-        gray: '#FBFBFB'
+    extend: {
+      colors: {
+        brand: {
+          zero: '#4458BE',
+          one: '#4B61D1',
+          two: '#7889DD',
+          three: '#EFF2FF',
+          four: '#F8F6F0',
+          five: '#DDBEA9',
+          six: '#CB997E',
+          white: '#FDFDFD',
+          gray: '#FBFBFB'
+        },
       },
-    },
+    }, 
     fontFamily: {
       sans: ['Lato', 'sans-serif'],
       serif: ['la_luxes_serifregular', 'serif'],
