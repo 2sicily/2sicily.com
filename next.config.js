f8e98dbaaead5cd4e3e6de8816d47dff92f57b35
2sicily <2sicily.tech@gmail.com>
2021-08-25 08:27:24 +0100

    changing git user

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withTM({
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ['en-gb'],
+    locales: ['en-gb', 'it-it'],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'en-gb',
