55d2b411cdf85eede1186b64d4eff3b7cc126451
fi3nn <finn@finnelliott.com>
2022-03-09 15:21:25 +0000

    altering external links in headline stats

--- slices/HeadlineStats/index.js
+++ slices/HeadlineStats/index.js
@@ -36,12 +36,13 @@ const MySlice = ({ slice }) => (
       <div className="font-serif text-lg text-black">{slice.primary.callToActionHeading}</div>
       <div className="text-base font-light pt-2 pb-3 text-black">{slice.primary.callToActionDescription}</div>
       <div className="flex w-full justify-center">
-      {slice.primary.callToActionLink && slice.primary.callToActionLinkText && 
-      <Link href={hrefResolver(slice.primary.callToActionLink)}>
+      {slice.items.map((item) => (
+      item.externalLink && item.externalLinkText && 
+      <Link href={hrefResolver(item.externalLink)}>
           <a className="text-lg text-brand-one font-sans italic hover:text-opacity-70 flex items-center">
           <div>
-          {slice.primary.callToActionLinkText &&
-            slice.primary.callToActionLinkText
+          {item.externalLinkText &&
+            item.externalLinkText
           }
           </div>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -49,7 +50,7 @@ const MySlice = ({ slice }) => (
           </svg>
           </a>
       </Link>
-      }
+      ))}
       </div>
     </div>
   </div>
